4ff8715a77ccddbe4d77beebd412ae4571d58c69
Dani Bw <danibroadwater@outlook.com>
2020-09-22 23:29:45 -0500

    added some style

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const button = document.querySelector('.submit-button')
 button.addEventListener('click', async () => {
   let userInput = input.value;
   const response = await axios.get(`${BASE_URL}/search/${userInput}`); //endpoint able to search character by name
-  console.log(response.data.results[0]);
+  console.log(response.data.results[0].Object);
   statsList(response.data.results[0]) // result wanted to get information of the first in the list
 
 });
@@ -33,6 +33,41 @@ let statsList = hero => {
 
   heroContainer.appendChild(heroImg)
 
+  const biography = document.createElement('div')
+  biography.className = "sh-biography"
+  heroContainer.appendChild(biography)
+
+  const biographyKeys = document.createElement('p')
+  biographyKeys.className = "biography-entries"
+  const bioKeys = `${Object.keys(hero.biography)}`.split(',').join('\n')
+  console.log(bioKeys);
+  const bioValues = `${Object.values(hero.biography)}`.split(',').join('\n')
+  console.log(bioValues);
+  biographyKeys.innerText = (`${bioKeys}` + `:${bioValues}`)
+  // const bioMap = new Map()
+  // for (let i = 0; i < bioKeys.length; i++) {
+  //   bioMap.set(bioKeys[i], bioValues[i])
+  // }
+  // console.log(bioMap);
+
+  // for (let key of bioMap.bioKeys()) {
+  //   document.write(key + ' = ' + bioMap.get(key) + "<br>")
+  // }
+
+  // biographyKeys.innerHTML = `${Object.keys(hero.biography)}`.split(',').join(`${Object.values(hero.biography)}`)
+  // biographyKeys.innerHTML = hero.forEach(element => {
+  //   `${Object.keys(hero.biography)}`.split(',').join(`<br>:${Object.values(hero.biography)}`)
+  // });
+  console.log(biographyKeys);
+  biography.appendChild(biographyKeys)
+
+  // const biographyValues = document.createElement('p')
+  // biographyValues.className = "biography-values"
+  // biographyValues.innerHTML = `${Object.values(hero.biography)}`.split(`,`).join('\n ')
+  // console.log(biographyValues);
+  // biography.appendChild(biographyValues)
+
+
   const powerStats = document.createElement('div')
   powerStats.className = "power-stats"
 
@@ -46,13 +81,13 @@ let statsList = hero => {
   // let powerStats = array
   // powerStats.append(array)
   powerStats.appendChild(powerStatsKeys)
-  console.log(powerStatsKeys);
+  // console.log(powerStatsKeys);
 
   const powerStatsValues = document.createElement('p')
   powerStatsValues.className = "powerstats-values"
   powerStatsValues.innerHTML = `${Object.values(hero.powerstats)}`.split(`,`).join('\n ')
   powerStats.appendChild(powerStatsValues)
-  console.log(powerStatsValues);
+  // console.log(powerStatsValues);
 
 
   heroDisplay.appendChild(heroContainer)
