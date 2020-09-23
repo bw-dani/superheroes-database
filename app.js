29fb6e55405f0f2e154748ab3a6a697d32f4983f
Dani Bw <danibroadwater@outlook.com>
2020-09-23 15:42:11 -0500

    style

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const button = document.querySelector('.submit-button')
 button.addEventListener('click', async () => {
   let userInput = input.value;
   const response = await axios.get(`${BASE_URL}/search/${userInput}`); //endpoint able to search character by name
-  console.log(response.data.results[0].Object);
+  console.log(response.data.results[0]);
   statsList(response.data.results[0]) // result wanted to get information of the first in the list
 
 });
@@ -27,69 +27,65 @@ let statsList = hero => {
   heroName.innerHTML = `${hero.name}`;
   section.append(heroName);
 
+
+  //    **** IMAGE ****
   const heroImg = document.createElement('img')
   heroImg.className = "hero-img"
   heroImg.setAttribute('src', hero.image.url)
 
   heroContainer.appendChild(heroImg)
 
+
+
+  //     ****  BIOGRAPHY  ****
   const biography = document.createElement('div')
   biography.className = "sh-biography"
   heroContainer.appendChild(biography)
 
   const biographyKeys = document.createElement('p')
   biographyKeys.className = "biography-entries"
-  const bioKeys = `${Object.keys(hero.biography)}`.split(',').join('\n')
-  console.log(bioKeys);
-  const bioValues = `${Object.values(hero.biography)}`.split(',').join('\n')
-  console.log(bioValues);
-  biographyKeys.innerText = (`${bioKeys}` + `:${bioValues}`)
-  // const bioMap = new Map()
-  // for (let i = 0; i < bioKeys.length; i++) {
-  //   bioMap.set(bioKeys[i], bioValues[i])
-  // }
-  // console.log(bioMap);
+  // const bioKeys = `${Object.keys(hero.biography)}`.split(',').join('\n')
+  // console.log(bioKeys);
+  // const bioValues = `${Object.values(hero.biography)}`.split(',').join('\n')
+  // console.log(bioValues);
+  // biographyKeys.innerText = (`${bioKeys}` + `:${bioValues}`)
 
-  // for (let key of bioMap.bioKeys()) {
-  //   document.write(key + ' = ' + bioMap.get(key) + "<br>")
-  // }
+  Object.entries(hero.biography).forEach((key, value) => {
+    // console.log(key, value);
+    biographyKeys.innerText += JSON.stringify((`${key}`).split(',').join(': ')).split('"').join('\r\n')
+  })
 
-  // biographyKeys.innerHTML = `${Object.keys(hero.biography)}`.split(',').join(`${Object.values(hero.biography)}`)
-  // biographyKeys.innerHTML = hero.forEach(element => {
-  //   `${Object.keys(hero.biography)}`.split(',').join(`<br>:${Object.values(hero.biography)}`)
-  // });
   console.log(biographyKeys);
   biography.appendChild(biographyKeys)
 
-  // const biographyValues = document.createElement('p')
-  // biographyValues.className = "biography-values"
-  // biographyValues.innerHTML = `${Object.values(hero.biography)}`.split(`,`).join('\n ')
-  // console.log(biographyValues);
-  // biography.appendChild(biographyValues)
 
+  //  **** POWERSTATS ****
 
   const powerStats = document.createElement('div')
   powerStats.className = "power-stats"
-
   heroContainer.appendChild(powerStats)
 
   const powerStatsKeys = document.createElement('p');
   powerStatsKeys.className = "powerstats-keys"
-  powerStatsKeys.innerHTML = `${Object.keys(hero.powerstats)}`.split(`,`).join(': \n ')
+  // powerStatsKeys.innerHTML = `${Object.keys(hero.powerstats)}`.split(`,`).join(': \n ')
   // powerStats.innerHTML = `${Object.keys(hero.powerstats)} ${Object.values(hero.powerstats)}`.split(`,`).join('\n')
   // let array = powerStats.innerHTML.split(',').join('\n')
   // let powerStats = array
   // powerStats.append(array)
-  powerStats.appendChild(powerStatsKeys)
+
   // console.log(powerStatsKeys);
 
-  const powerStatsValues = document.createElement('p')
-  powerStatsValues.className = "powerstats-values"
-  powerStatsValues.innerHTML = `${Object.values(hero.powerstats)}`.split(`,`).join('\n ')
-  powerStats.appendChild(powerStatsValues)
+  // const powerStatsValues = document.createElement('p')
+  // powerStatsValues.className = "powerstats-values"
+  // powerStatsValues.innerHTML = `${Object.values(hero.powerstats)}`.split(`,`).join('\n ')
+
   // console.log(powerStatsValues);
 
-
+  Object.entries(hero.powerstats).forEach((key, value) => {
+    powerStatsKeys.innerText += (`${key} \n`).split(',').join(':')
+  })
+  // powerStats.appendChild(powerStatsValues)
+  powerStats.appendChild(powerStatsKeys)
   heroDisplay.appendChild(heroContainer)
 
 }
